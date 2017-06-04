da7f5b6447db678b61a7b8422d52646558f2d0f5
Jack <z2589906@gmail.com>
2017-06-04 11:33:59 +0800

    Start writing curriculum's part

---
+++ Curriculum/Controllers/CurriculumController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Curriculum.Controllers
+{
+    public class CurriculumController : Controller
+    {
+        // GET: Curriculum
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
