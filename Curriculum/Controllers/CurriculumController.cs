906d5ef1551fbd5a2f351ca04e733ea0b875378b
Jack <z2589906@gmail.com>
2017-06-06 02:10:21 +0800

    - 資料庫建立 - 課表新增與刪除 - 基本註冊功能

--- Curriculum/Controllers/CurriculumController.cs
+++ Curriculum/Controllers/CurriculumController.cs
@@ -31,7 +31,38 @@ namespace Curriculum.Controllers
             SqlDataAdapter ad = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             ad.Fill(dt);
+            conn.Close();
             return JsonConvert.SerializeObject(dt);
+        }
+
+        public void AddClass(string row, string col, string teacher, string className, string classRoom) {
+            SqlConnection conn = new SqlConnection(connectString);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "INSERT INTO Curriculum VALUES (@row, @col, @account, @className, @teacher, @classRoom)";
+            cmd.Parameters.Add("@row", SqlDbType.NVarChar, 15).Value = row;
+            cmd.Parameters.Add("@col", SqlDbType.NVarChar, 15).Value = col;
+            cmd.Parameters.Add("@account", SqlDbType.VarChar, 15).Value = (string)Session["account"];
+            cmd.Parameters.Add("@className", SqlDbType.NVarChar, 10).Value = className;
+            cmd.Parameters.Add("@teacher", SqlDbType.NVarChar, 10).Value = teacher;
+            cmd.Parameters.Add("@classRoom", SqlDbType.NVarChar, 10).Value = classRoom;
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void DeleteClass(string row, string col)
+        {
+            SqlConnection conn = new SqlConnection(connectString);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "DELETE FROM Curriculum WHERE row=@row AND col=@col";
+            cmd.Parameters.Add("@row", SqlDbType.VarChar, 10).Value = row;
+            cmd.Parameters.Add("@col", SqlDbType.VarChar, 10).Value = col;
+            cmd.ExecuteNonQuery();
+            conn.Close();
         }
     }
 }
